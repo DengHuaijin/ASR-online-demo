33f2d3fc7d14625a8117294bb913b57f9c2d1eac
DengHuaijin <denghuaijin@yahoo.co.jp>
2021-01-03 03:51:54 +0900

    deleted

--- ASR/demo/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [url(r"demo/", views.demo),
-               url(r"demo/search/", views.search)]
--- demo/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def demo(request):
-    context = {}
-    context["hello"] = "hello world"
-    return render(request, "demo/index.html", context)
-
