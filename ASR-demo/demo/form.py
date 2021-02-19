90d1cd7fc8c5f215f10010803f473dbd69208727
DengHuaijin <denghuaijin@yahoo.co.jp>
2021-02-20 00:22:54 +0800

    bug fixed

--- ASR-demo/demo/form.py
+++ ASR-demo/demo/form.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import logging
+from datetime import datetime
 logger = logging.getLogger(__name__)
 from django.shortcuts import redirect, render
 from django.http import HttpResponseRedirect, HttpResponse
@@ -29,9 +30,10 @@ def handle_uploaded_file(f, filename):
 @csrf_exempt
 def upload_file(request):
     if request.method == "POST":
+        curtime = datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
         form = UploadFileForm(request.POST, request.FILES)
-        audio_file = handle_uploaded_file(request.FILES['audiofile'], "uploadAudio.wav")
-        text = tasks.recognition(settings.MEDIA_ROOT, "upload")
+        audio_file = handle_uploaded_file(request.FILES['audiofile'], "upload/" + curtime+".wav")
+        text = tasks.recognition_ds2(audio_file)
         # text = "This it's the online speech recognition demo"
         # text = tasks.recognition_e2e(audio_file)
         context = {"text": text}
@@ -44,12 +46,13 @@ def upload_file(request):
 def record_file(request):
     if request.method == "POST":
         language = request.POST["language"]
+        curtime = datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
         if language == "en":
-            audio_file = handle_uploaded_file(request.FILES['audio'], "recordAudio.wav")
+            audio_file = handle_uploaded_file(request.FILES['audio'], "record/" + curtime+".wav")
             # text = "English online speech recognition demo"
             text = tasks.recognition_ds2(audio_file)
         elif language == "jp":
-            audio_file = handle_uploaded_file(request.FILES['audio'], "kaldi/recordAudio.wav")
+            audio_file = handle_uploaded_file(request.FILES['audio'], "kaldi/kaldiAudio.wav")
             text = tasks.recognition_kaldi()
             # text = "Japanese online speech recognition demo"
         else:
--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 - [采坑记录1 post文件上传](https://blog.csdn.net/dhj_tsukuba/article/details/112167117)
 
+- [采坑记录2 kaldi-nnet3模型在线解码](https://blog.csdn.net/dhj_tsukuba/article/details/113620910)
+
 ## Django+JS (VS2017 Project)
 
 ## ![demo](https://media.giphy.com/media/GiVhZFsRwYHs0ygnPM/giphy.gif)
 
 ## ![](https://media.giphy.com/media/9kX2qNBKRr07EKKQ2o/giphy.gif)
-
